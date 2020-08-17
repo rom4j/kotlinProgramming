15cd8c6d1b3fbeceda5a8ccbe74cf0ca8ea800d1
Julio Ramirez <rrodriguez.julio@gmail.com>
2020-08-16 23:35:44 -0500

    finish chapter 2

--- src/TypeIntro.kt
+++ src/TypeIntro.kt
@@ -1,6 +1,11 @@
+
+const val MAX_EXPERIENCE: Int = 5000
+
 fun main(arg: Array<String>) {
-  var experiencePoint: Int = 5
-  println(experiencePoint)
+  val playerName = "Estragon"
+  var experiencePoint = 5
   experiencePoint += 5
   println(experiencePoint)
+  println(playerName)
+  println(MAX_EXPERIENCE)
 }
